2b58d95e3e236ecb55df43f9be6180d5038b8300
janniksinnervero <gabrielegilo.malaspina@galileiostiglia.edu.it>
2026-01-11 17:14:41 +0000

    uyftyf

--- s.c
+++ s.c
@@ -5,11 +5,21 @@ int main () {
     int p=0;
     int i=0;
 
-    printf("Inserisci un valore intero: ");
+    printf("Inserisci un valore intero positivo: ");
     scanf("%d", &n1);
 
-    printf("\nInserisci un valore intero: ");
+    while (n1<0) {
+        printf("Il valore inserito non è valido. Riprova: ");
+        scanf("%d", &n1);
+    }
+
+    printf("\nInserisci un valore intero positivo: ");
     scanf("%d", &n2);
+
+    while (n2<0) {
+        printf("Il valore inserito non è valido. Riprova: ");
+        scanf("%d", &n2);
+    }
 
     for(i=0; i < n2; i++) {
         p=p+n1;
---
+++ treq.c
@@ -0,0 +1,32 @@
+/*
+  l'utente inserisce un carattere c e un intero l tra 1 e 70 (controllare);
+  stampare, con due for annidati, un triangolo equilatero di lato l;
+*/
+
+#include <stdio.h>
+int main (){
+
+    int l=0;
+    char c;
+
+    printf("Inserisci un valore intero compreso tra 1 e 70: ");
+    scanf("%d", &l);
+    while (l>70 || l<0) {
+        printf("Valore inserito non valido. Riprova: ");
+        scanf("%d", &l);
+    }
+
+    printf("Seleziona un carattere: ");
+    scanf(" %c", &c);
+
+    for (int cnt_l=0; cnt_l<l; cnt_l++) {
+        printf("%c", c);
+
+        for(int cnt_c; cnt_c<cnt_l; cnt_c++){
+            printf("%c", c);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
