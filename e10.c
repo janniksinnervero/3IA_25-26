513104cb73a9594e8cff6508496e0ad06418a46a
janniksinnervero <gabrielegilo.malaspina@galileiostiglia.edu.it>
2026-02-11 11:59:15 +0000

    zdf

--- e10.c
+++ e10.c
@@ -6,22 +6,25 @@ Utilizzo della forma iterativa do-while.
 
 
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
 int main (void) {
 
     int scelta=1;
     int num=0;
     int div=0;
     int cnt=0;
+    int temp=0;
 
     printf("MENU\n");
     printf("1 -> Visualizza i divisori\n");
     printf("2 -> Verifica numero primo\n");
     printf("3 -> Verifica numero perfetto\n");
-    printf("0 -> Terimina programma\n");
     printf("4 -> Genera N numeri casuali <1000\n");
-    printf("5 -> Genera n numeri dispari <1000\n");
-    printf("6 -> Genera n numeri casuali crescenti <9999\n");
+    printf("5 -> Genera N numeri dispari <1000\n");
+    printf("6 -> Genera N numeri casuali crescenti <9999\n");
     printf("7 -> Genera un numero casuale, determina il successivo numero primo\n");
+    printf("0 -> Terimina programma\n");
 
     while(scelta!=0){
 
@@ -98,6 +101,57 @@ int main (void) {
 
             break;
 
+            case 4: {
+                printf("\nInserisci un valore intero positivo: ");
+                scanf("%d", &num);
+                while (num%1==0 && num<0){
+                    printf("\nValore non valido. Riprova: ");
+                    scanf("%d", &num);
+                }
+                const int max=1000;
+                srand(time(NULL));
+
+                do{
+                    temp=1+rand() %max;
+                    printf("%d ", temp);
+                    cnt=cnt+1;
+                }   
+                while (cnt<num);
+            }
+
+            break;
+
+            case 5: {
+                printf("\nInserisci un valore intero positivo: ");
+                scanf("%d", &num);
+                while (num%1==0 && num<0){
+                    printf("\nValore non valido. Riprova: ");
+                    scanf("%d", &num);
+                }
+                const int max=1000;
+                srand(time(NULL));
+
+                do{
+                    temp=1+rand() %max;
+                    printf("%d ", temp);
+                    cnt=cnt+1;
+
+                    if(temp%2==0){
+                        temp=temp+1;
+                        printf("%d", temp);
+                    }
+
+                    else{
+                        printf("%d", temp);
+                    }
+
+                }   
+                while (cnt<num);
+
+            }
+
+            break;
+
             default:
         }
     }
@@ -105,4 +159,3 @@ int main (void) {
     printf("\nProgramma terminato.");
     return 0;
 }
-
---
+++ funz.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+/**
+ * verifica se un numero è primo oppure no
+ * @param int numero da testare
+ * @return boolean true/false
+ */
+bool ver_primo(int _n);
+
+/**
+ * Checks whether a numer is perfect or not
+ * @param int number to test
+ * @return boolean true/false
+ */
+bool ver_perf(int _n);
+
+
+
+int main (void){
+    int menu=0;
+
+    printf("Seleziona un' opzione: \n");
+    printf("1 -> Verifica un numero primo\n");
+    printf("2 -> Verifica un numero perfetto\n");
+    printf("3 -> Verifica un numero è una terna pitagorica\n");
+
+    scanf("%d", &menu);
+
+    switch(menu){
+
+        case 1: {
+            ver_primo;
+
+        }
+    }
+
+
+
+
+    return 0;
+}
+
+
+bool ver_primo(int _n){
+    int div=1;
+    int cnt=0;
+
+    printf("Inserisci un valore intero: ");
+    scanf("%d", &_n);
+
+    for(div=1; div<=_n; div++){
+        if (_n%div==0){
+            cnt++;
+        }
+    }
+
+    if(cnt<=2){
+        return(true);
+    }
+    else{
+        return(false);
+    }   
+}
+
+bool ver_perf(int _n){
+    int div=1;
+    int sum=0;
+    int _n=0;
+
+    printf("Inserisci un valore intero: ");
+    scanf("%d", &_n)
+
+    for(div=1; div<=_n; div++){
+        if (_n%div==0){
+            sum=sum+div
+        }
+
+        if(sum==_n){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+}
